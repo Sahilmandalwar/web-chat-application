54e9d90f7e02b0efd6b8acecbd72d235a0c86864
sahil Mandalwar <sahilmandalwar30@gmail.com>
2026-03-26 19:29:28 +0530

    added message controller having sendMessage getChatPartners getMessagesByUserId and GetAllContact

---
+++ backend/src/controllers/messageController.js
@@ -0,0 +1,107 @@
+import User from "../models/userModel.js";
+import Message from "../models/messageModel.js";
+import {v2 as cloudinary} from "cloudinary";
+export const getAllContact = async (req,res) => {
+    try {
+        const loggedInUserId = req.user._id;
+        const filteredUsers = await User.find({
+            _id : {
+                $ne: loggedInUserId
+            }
+        }).select("-password");
+
+        res.status(200).json(filteredUsers);
+    }catch(error){
+        console.log("Error while fetching Users: ",error);
+        res.status(500).json({message: "Internal Server Error"});
+    }
+}
+
+export const getMessagesByUserId = async (req,res) => {
+  
+    try {
+        const loggedInUserId = req.user._id;
+        const {id:userToChatId} = req.params;
+        const allMessages = await Message.find({
+            $or : [
+                {senderId: loggedInUserId, receiverId:userToChatId },
+                {receiverId: loggedInUserId, senderId:userToChatId },
+            ]
+        })
+
+        res.status(200).json(allMessages);
+
+
+    }catch(error) {
+        console.log("Error while Fetching Messages of user: ", error);
+        res.status(500).json({message: "Internal Server Error"})
+    }
+}
+
+export const sendMessage = async (req,res) => {
+    try {
+        const senderId = req.user._id;
+        const {id:receiverId} = req.params;
+        const {text, image, video} = req.body;
+
+        let imageURL;
+        let videoURL;
+        if(image) {
+            // upload the image to cloudinary
+            const uploadResponse = await cloudinary.uploader.upload(image);
+            imageURL = uploadResponse.secure_url;
+        }
+        if(video) {
+            const uploadResponse = await cloudinary.uploader.upload(video,{
+                resource_type: "video",
+            });
+            videoURL = uploadResponse.secure_url;
+        }
+
+        const newMessage = new Message({
+            senderId,
+            receiverId,
+            text,image:imageURL,video:videoURL
+        })
+
+        const savedMessage = await newMessage.save();
+
+        // todo: send message in real-time if user is online -socket.io
+        res.status(200).json({
+            message: "New Message created succefully",
+            newMessage
+        })
+
+    }catch(error){
+        console.log("Error while Fetching Messages of user: ", error.message);
+        res.status(500).json({message: "Internal Server Error"})
+    }
+}
+
+export const getChatPartners = async(req,res)=>{
+    try {
+        const userId = req.user._id;
+        const allMessagesOfLoggedInUser = await Message.find({
+            $or : [
+                {senderId : userId} ,
+                {receiverId: userId}
+            ]
+        })
+        const chatPartnerIds = allMessagesOfLoggedInUser.map((msg)=>{
+            return msg.senderId.toString() === userId.toString() ? msg.receiverId.toString() : msg.senderId.toString();
+        })
+        const chatPartners = await User.find({
+            _id: {
+                $in: 
+                    chatPartnerIds
+                
+            }
+        }).select("-password");
+
+        res.status(200).json(chatPartners)
+
+    }catch(error) {
+        console.log("Error occured while fetching chatPartners details: ", error.message);
+        res.status(500).json({message: "Internal Server Error"});
+    }
+}
