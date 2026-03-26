5fc0a2a3c7ae14cc9b9c765aa76e6b1b270fa6c1
sahil Mandalwar <sahilmandalwar30@gmail.com>
2026-03-26 19:30:47 +0530

    message route is created

--- backend/src/routes/message.js
+++ backend/src/routes/message.js
@@ -1,12 +1,18 @@
 import express from "express";
+import { getAllContact,getMessagesByUserId,sendMessage,getChatPartners } from "../controllers/messageController.js";
+import { protectRoute } from "../middleware/authMiddleware.js";
+import { arcjetProtection } from "../middleware/arcjetMiddleware.js";
 const router = express.Router();
 
-router.get("/",(req,res)=>{
-    res.send("all your message here...")
-})
-router.get("/send",(req,res)=>{
-    res.send("send your message here...")
-})
+// router.use(arcjetProtection)
+router.use(protectRoute)
+
+
+router.get("/contact",getAllContact);
+router.get("/chat",getChatPartners);
+router.post("/send/:id",sendMessage);
+router.get("/:id" ,getMessagesByUserId);
+
 
 
 export default router;
