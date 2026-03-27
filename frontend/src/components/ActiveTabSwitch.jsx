59f32cf06397f6471cb9def4ddfb31aa0d0c15af
sahil Mandalwar <sahilmandalwar30@gmail.com>
2026-03-27 20:14:02 +0530

    Add responsive auth pages and chat components

---
+++ frontend/src/components/ActiveTabSwitch.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ActiveTabSwitch = () => {
+  return (
+    <div>
+      activeTab
+    </div>
+  )
+}
+
+export default ActiveTabSwitch
---
+++ frontend/src/components/ContactList.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ContactList = () => {
+  return (
+    <div>
+      contactList
+    </div>
+  )
+}
+
+export default ContactList
